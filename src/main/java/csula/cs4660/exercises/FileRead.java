f2aa6316899763b7eaa5071ed3be4be0dbe052c5
SoulEaterGames <thebrunoexperience@gmail.com>
2016-08-28 09:52:11 -0700

    Initial commit

--- src/main/java/csula/cs4660/exercises/FileRead.java
+++ src/main/java/csula/cs4660/exercises/FileRead.java
@@ -1,18 +1,34 @@
 package csula.cs4660.exercises;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Introduction Java exercise to read file
  */
 public class FileRead {
     private int[][] numbers;
+    int row = 8;
+    int column = 5;
     /**
      * Read the file and store the content to 2d array of int
      * @param file read file
+     * @throws FileNotFoundException
      */
-    public FileRead(File file) {
+    public FileRead(File file) throws FileNotFoundException {
         // TODO: read the file content and store content into numbers
+
+        Scanner input = new Scanner(file);
+        String line = input.nextLine();
+        String[] n = line.split(" ");
+
+        for(int i = 0; i < row; i++){
+          for(int j = 0; j < column; j++){
+            numbers[i][j] = Integer.parseInt(n[j]);
+          }
+        }
+
     }
 
     /**
@@ -37,4 +53,9 @@ public class FileRead {
     public int sum(int lineNumber) {
         return 0;
     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
